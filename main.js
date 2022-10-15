0c974e49e39a584369dccc84c1b525d9f271ac81
linuxguy467 <me@matthem.codes>
2022-10-15 14:17:47 -0400

    Configure Content Security Policy for app

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
 const path = require("path");
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, session } = require("electron");
 
 const isDev = process.env.NODE_ENV !== "production";
 const isMac = process.platform === "darwin";
 
+function setCSP() {
+  session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+    callback({
+      responseHeaders: {
+        ...details.responseHeaders,
+        "Content-Security-Policy": ["script-src 'self'"],
+      },
+    });
+  });
+}
+
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: "Image Resizer",
-    width: isDev ? 1000: 500,
+    width: isDev ? 1000 : 500,
     height: 600,
   });
 
   if (isDev) {
     mainWindow.webContents.openDevTools();
   }
+
+  setCSP();
 
   mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 }
