4c88d20f1b7bf6acd6d465a8e0c976fc99f448b4
linuxguy467 <me@matthem.codes>
2022-10-15 13:30:19 -0400

    Add option for devtools

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 const path = require("path");
 const { app, BrowserWindow } = require("electron");
 
+const isDev = process.env.NODE_ENV !== "production";
 const isMac = process.platform === "darwin";
 
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: "Image Resizer",
-    width: 500,
+    width: isDev ? 1000: 500,
     height: 600,
   });
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
 
   mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 }
